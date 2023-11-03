fb9639e70df6bd70650172d822b412ccc4bc7d1a
Thushara Bandara <thusharathemalbandara@gmail.com>
2023-11-03 23:37:52 +0800

    changes

--- Nwsdb.Web/ClientApp/src/app/app.module.ts
+++ Nwsdb.Web/ClientApp/src/app/app.module.ts
@@ -6,16 +6,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './web/shared/nav-menu/nav-menu.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
-import { alarm, alarmFill, alignBottom } from 'ngx-bootstrap-icons';
 import { LandsAllMainComponent } from './web/lands/all/lands-all-main/lands-all-main.component';
-
-
-const icons = {
-  alarm,
-  alarmFill,
-  alignBottom
-};
 
 @NgModule({
   declarations: [
@@ -29,9 +20,6 @@ const icons = {
     RouterModule,
     FormsModule,
     AppRoutingModule,
-    NgxBootstrapIconsModule.pick(allIcons),
-    NgxBootstrapIconsModule.pick(icons),
-
   ],
   providers: [],
   bootstrap: [AppComponent],
